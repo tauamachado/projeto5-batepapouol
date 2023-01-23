895d9bb36742c178b318cac635f939907765768f
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-01-23 08:06:59 -0300

    implementação função de entrar e refresh a cada 3seg do chat

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 let messages = [];
+let message = {};
+const chat = document.querySelector('.messages');
+let nome = prompt('Qual o seu nome?');
 
-const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+function joinChat() {
+	axios
+		.post('https://mock-api.driven.com.br/api/v6/uol/participants', {
+			name: nome,
+		})
+		.then(messageChecker)
+		.catch(() => {
+			window.location.reload();
+		});
+}
+joinChat();
 
-promise.then(messageArrived);
+function messageChecker() {
+	axios
+		.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+		.then(messageArrived);
+}
 
 function messageArrived(res) {
 	messages = res.data;
+	chat.innerHTML = '';
 	messageRender();
 }
 
 function messageRender() {
-	const chat = document.querySelector('.messages');
-
 	for (let i = 0; i < messages.length; i++) {
 		if (messages[i].type === 'status') {
 			chat.innerHTML += `
@@ -33,4 +49,25 @@ function messageRender() {
       `;
 		}
 	}
+	document.querySelector('.message:last-child').scrollIntoView();
 }
+
+function sendMessage() {
+	const text = document.querySelector('input').value;
+
+	const promise = axios.post(
+		'https://mock-api.driven.com.br/api/v6/uol/messages',
+		{
+			from: nome,
+			to: 'Todos',
+			text: text,
+			type: 'message',
+		}
+	);
+	promise.then(messageChecker);
+	promise.catch((error) => {
+		console.log(error);
+	});
+}
+
+setInterval(messageChecker, 3000);
